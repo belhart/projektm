546c8c78255ad733f345e8b7670ebdfae989db75
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-02-23 12:05:09 +0100

    final test

--- React/example-app/src/App.js
+++ React/example-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to rrreload.
+          Edit <code>src/App.js</code> and save to rrreloadxd.
         </p>
         <a
           className="App-link"
